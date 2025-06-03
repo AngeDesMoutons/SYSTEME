d9bb33b00451124417d7d303cdd7a1fbaf1a175d
BuildTools <unconfigured@null.spigotmc.org>
2025-06-03 21:26:16 +0200

    update data

--- data.php
+++ data.php
@@ -1,7 +1,22 @@
 <?php
-// filepath: c:\Users\Utilisateur\OneDrive\Bureau\site internet\data.php
+<?php
 header('Content-Type: application/json');
 $dataFile = 'data.json';
+
+// Valeurs par défaut si le fichier n'existe pas
+$defaultData = json_encode([
+    "xp" => 0,
+    "maxXp" => 100,
+    "level" => 0,
+    "tasks" => [],
+    "quests" => [],
+    "malus" => []
+], JSON_PRETTY_PRINT);
+
+// Si le fichier n'existe pas, on le crée avec les valeurs par défaut
+if (!file_exists($dataFile)) {
+    file_put_contents($dataFile, $defaultData);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     readfile($dataFile);
